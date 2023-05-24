903bb3257081ffdf0636838fd4a0ebb0beb18007
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-05-24 20:05:16 +0300

    async wait

--- contacts.js
+++ contacts.js
@@ -6,97 +6,85 @@ const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 console.log("Contacts file path:", contactsPath);
 
-function listContacts() {
-  return fs
-    .readFile(contactsPath)
-    .then((data) => {
-      const contacts = JSON.parse(data);
-      return { message: "listContacts: Here is current list of users in Data Base", contacts };
-    })
-    .catch((err) => {
-      console.error(err.message);
-      return { message: "listContacts: Error occurred while reading contacts file", contacts: null };
-    });
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return { message: "listContacts: Here is current list of users in Data Base", contacts };
+  } catch (err) {
+    console.error(err.message);
+    return { message: "listContacts: Error occurred while reading contacts file", contacts: null };
+  }
 }
 
-function getContactById(contactId) {
-  return fs
-    .readFile(contactsPath)
-    .then((data) => {
-      const contacts = JSON.parse(data);
-      const contact = contacts.find((contact) => contact.id === contactId);
-      if (contact) {
-        const message = `getContactById: Contact with ID ${contactId} found`;
-        return { message, contact };
-      }
-      return { message: `getContactById: Contact with ID ${contactId} not found`, contact: null };
-    })
-    .catch((err) => {
-      console.log(err.message);
-      return { message: "getContactById: Error occurred while reading contacts file", contact: null };
-    });
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === contactId);
+    if (contact) {
+      const message = `getContactById: Contact with ID ${contactId} found`;
+      return { message, contact };
+    }
+    return { message: `getContactById: Contact with ID ${contactId} not found`, contact: null };
+  } catch (err) {
+    console.log(err.message);
+    return { message: "getContactById: Error occurred while reading contacts file", contact: null };
+  }
 }
 
-function removeContact(contactId) {
-  return fs
-    .readFile(contactsPath)
-    .then((data) => {
-      const contacts = JSON.parse(data);
-      const contact = contacts.find((contact) => contact.id === contactId);
-      if (contact) {
-        const filteredContacts = contacts.filter((contact) => contact.id !== contactId);
-        const updatedData = JSON.stringify(filteredContacts);
-        return fs
-          .writeFile(contactsPath, updatedData)
-          .then(() => {
-            return { message: `removeContact: Contact with ID ${contactId} removed`, contact };
-          })
-          .catch((err) => {
-            console.log(err.message);
-            return { message: "removeContact: Error occurred while writing to contacts file", contact: null };
-          });
-      } else {
-        return { message: `removeContact: Contact with ID ${contactId} not found`, contact: null };
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === contactId);
+    if (contact) {
+      const filteredContacts = contacts.filter((contact) => contact.id !== contactId);
+      const updatedData = JSON.stringify(filteredContacts);
+      try {
+        await fs.writeFile(contactsPath, updatedData);
+        return { message: `removeContact: Contact with ID ${contactId} removed`, contact };
+      } catch (err) {
+        console.log(err.message);
+        return { message: "removeContact: Error occurred while writing to contacts file", contact: null };
       }
-    })
-    .catch((err) => {
-      console.log(err.message);
-      return { message: "removeContact: Error occurred while reading contacts file", contact: null };
-    });
+    } else {
+      return { message: `removeContact: Contact with ID ${contactId} not found`, contact: null };
+    }
+  } catch (err) {
+    console.log(err.message);
+    return { message: "removeContact: Error occurred while reading contacts file", contact: null };
+  }
 }
 
-function addContact(name, email, phone) {
-  return fs
-    .readFile(contactsPath)
-    .then((data) => {
-      const contacts = JSON.parse(data);
-      const id = shortid();
-      const sameName = contacts.find((contact) => contact.name === name);
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const id = shortid();
+    const sameName = contacts.find((contact) => contact.name === name);
 
-      if (!sameName) {
-        const newContact = { name, email, phone, id };
-        contacts.push(newContact);
-        const updatedData = JSON.stringify(contacts);
+    if (!sameName) {
+      const newContact = { name, email, phone, id };
+      contacts.push(newContact);
+      const updatedData = JSON.stringify(contacts);
 
-        return fs
-          .writeFile(contactsPath, updatedData)
-          .then(() => {
-            const message = `addContact: Contact with ID ${id} added`;
-            return { message, contacts };
-          })
-          .catch((err) => {
-            console.log(err.message);
-            return { message: "addContact: Error occurred while writing to contacts file", contacts: null };
-          });
-      } else {
-        const message = `addContact: Contact with name: ${name} already exists`;
-        return { message, contacts: null };
+      try {
+        await fs.writeFile(contactsPath, updatedData);
+        const message = `addContact: Contact with ID ${id} added`;
+        return { message, contacts };
+      } catch (err) {
+        console.log(err.message);
+        return { message: "addContact: Error occurred while writing to contacts file", contacts: null };
       }
-    })
-    .catch((err) => {
-      console.log(err.message);
-      return { message: "addContact: Error occurred while reading contacts file", contacts: null };
-    });
+    } else {
+      const message = `addContact: Contact with name: ${name} already exists`;
+      return { message, contacts: null };
+    }
+  } catch (err) {
+    console.log(err.message);
+    return { message: "addContact: Error occurred while reading contacts file", contacts: null };
+  }
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,69 +1,70 @@
 const contacts = require("./contacts");
 const argv = require("yargs").argv;
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      contacts
-        .listContacts()
-        .then((data) => {
+async function invokeAction({ action, id, name, email, phone }) {
+  try {
+    switch (action) {
+      case "list":
+        try {
+          const data = await contacts.listContacts();
           if (data.contacts !== null) {
             console.log(data.message);
             console.table(data.contacts);
           } else {
             console.log(data.message);
           }
-        })
-        .catch((err) => {
+        } catch (err) {
           console.error(err.message);
-        });
-      break;
+        }
+        break;
 
-    case "get":
-      contacts
-        .getContactById(id)
-        .then((data) => {
+      case "get":
+        try {
+          const data = await contacts.getContactById(id);
           if (data.contact !== null) {
             console.log(data.message);
             console.table(data.contact);
           } else {
             console.log(data.message);
           }
-        })
-        .catch((err) => {
+        } catch (err) {
           console.error(err.message);
-        });
-      break;
+        }
+        break;
 
-    case "add":
-      contacts
-        .addContact(name, email, phone)
-        .then((data) => {
+      case "add":
+        try {
+          const data = await contacts.addContact(name, email, phone);
           if (data.contacts !== null) {
             console.log(data.message);
             console.table(data.contacts);
           } else {
             console.log(data.message);
           }
-        })
-        .catch((err) => {
+        } catch (err) {
           console.error(err.message);
-        });
-      break;
-
-    case "remove":
-      contacts.removeContact(id).then((data) => {
-        if (data.contact !== null) {
-          console.log(data.message);
-          console.table(data.contact);
-        } else {
-          console.log(data.message);
         }
-      });
-      break;
+        break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      case "remove":
+        try {
+          const data = await contacts.removeContact(id);
+          if (data.contact !== null) {
+            console.log(data.message);
+            console.table(data.contact);
+          } else {
+            console.log(data.message);
+          }
+        } catch (err) {
+          console.error(err.message);
+        }
+        break;
+
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
